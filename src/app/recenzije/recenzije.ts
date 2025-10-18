b7e6fda3a5dea6da4bf32bd9491cbccae5261829
kiwi0610 <radonjicv004@gmail.com>
2025-10-18 19:16:43 +0200

    update NE RADI

--- src/app/recenzije/recenzije.ts
+++ src/app/recenzije/recenzije.ts
@@ -1,34 +1,54 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { UserService } from '../services/user.service';
-import { Details } from "../details/details";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recenzije',
-  imports: [Details],
+  imports: [CommonModule],
   templateUrl: './recenzije.html',
-  styleUrl: './recenzije.css'
+  styleUrls: ['./recenzije.css']
 })
-export class Recenzije {
+export class Recenzije implements OnInit {
 
   protected reviews = signal<any[]>([]);
+
+  ngOnInit() {
+    this.loadReviews();
+  }
 
   loadReviews() {
     const allReviews = JSON.parse(localStorage.getItem('reviews') || '[]');
     const activeUser = UserService.getActiveUser();
-    this.reviews.set(allReviews.filter((r: any) => r.userEmail === activeUser.email));
+    const userReviews = allReviews.filter((r: any) => r.userEmail === activeUser.email);
+    this.reviews.set(userReviews);
   }
 
-  // prosečna ocena za sve korisnike
-  // getAverageRating(toyId: any): number {
-  //  const allReviews = JSON.parse(localStorage.getItem('reviews') || '[]');
-   // const ratings = allReviews
-    //  .filter((r: any) => r.toyId === toyId && r.ratingValue > 0)
-     // .map((r: any) => r.ratingValue);
+  getItemPrice(item: any) {
+    return (item.amount ?? 1) * (item.price ?? 0);
+  }
 
-    //if (ratings.length === 0) return 0;
-   // return ratings.reduce((a, b) => a + b, 0) / ratings.length;
-  //}
+  getStatusClass(status: string) {
+    switch (status) {
+      case 'paid': return 'status-paid';
+      case 'cancelled': return 'status-canceled';
+      default: return '';
+    }
+  }
 
+  getAverageRating(toyId: any): number {
+    const allReviews = JSON.parse(localStorage.getItem('reviews') || '[]');
+    const ratings = allReviews
+      .filter((r: any) => r.toyId === toyId && r.ratingValue > 0)
+      .map((r: any) => r.ratingValue);
+    if (ratings.length === 0) return 0;
+    return ratings.reduce((a: number, b: number) => a + b, 0) / ratings.length;
+  }
+
+  updateRating(item: any, newValue: number) {
+    item.ratingValue = newValue;
+    item.updatedAt = new Date();
+    UserService.addReview(item); // ažurira u localStorage
+    alert(`Promenili ste ocenu za ${item.toyName} na ${newValue} ⭐`);
+  }
 
 }
-
